e18117c1bfb8dc593c74f39db40b65d1a7d068c6
BenM-2 <benmarples140@gmail.com>
2026-09-17 18:48:26 +0100

    added basic validation

--- main.c
+++ main.c
@@ -160,9 +160,9 @@ void print_board(const TTT_t *const arr, const size_t grid_size)
         {
             int index = (r * grid_size) + c;
             char x = get_player_char(arr[index]);
-            printf("%c,", x);
+            printf("\t%c\t", x);
         }
-        printf("\n");
+        printf("\n\n");
     }
 }
 
@@ -171,16 +171,21 @@ void reset_board(TTT_t *arr, size_t length)
     memset(arr, EMPTY, length);
 }
 
-#define ROW_X_wincheck(arr, row, width)                     \
-    ((arr)[(row) * (width)] != EMPTY && \
-    (arr)[(row) * (width)] == (arr)[1 + (row) * (width)] && \
-    (arr)[1 + (row) * (width)] == (arr)[2 + (row) * (width)])
+#define ROW_X_wincheck(arr, row, width)                      \
+    ((arr)[(row) * (width)] != EMPTY &&                      \
+     (arr)[(row) * (width)] == (arr)[1 + (row) * (width)] && \
+     (arr)[1 + (row) * (width)] == (arr)[2 + (row) * (width)])
+
+#define COL_X_wincheck(arr, col, width)        \
+    ((arr)[(col)] != EMPTY &&                  \
+     (arr)[(col)] == (arr)[(col) + (width)] && \
+     (arr)[(col) + (width)] == (arr)[(col) + (2) * (width)])
 
 TTT_t checkwin(const TTT_t *const arr, const int grid_size)
 {
     for (int i = 0; i < grid_size; i++)
     {
-        if (ROW_X_wincheck(arr, i, grid_size))
+        if (ROW_X_wincheck(arr, i, grid_size) || COL_X_wincheck(arr, i,grid_size))
         {
             return arr[i];
         }
@@ -199,20 +204,39 @@ void TicTacToe(int grid_size)
     // Play in loop
     while (win == EMPTY)
     {
-        // Print board
-        print_board(arr, grid_size);
 
         // Take input
-        int player_input = 0;
-        printf("Enter Position: ");
-        scanf("%d", &player_input);
+        int player_input = -1;
+        int fail_count = 0;
+        bool input_valid = false;
+
+        while (!input_valid)
+        {
+            // Print board
+            print_board(arr, grid_size);
+
+            if (fail_count > 0)
+            {
+                printf("\nINVALID INPUT\n\n");
+            }
+            printf("Player %c\n", get_player_char(p1_turn ? ZERO : CROSS));
+            printf("Enter Position: ");
+            scanf("%d", &player_input);
+
+            if (arr[player_input] == EMPTY && (player_input > 0 || player_input < grid_size * grid_size))
+            {
+                input_valid = true;
+            }
+
+            fail_count++;
+        }
         arr[player_input] = p1_turn ? ZERO : CROSS;
         p1_turn = !p1_turn;
         // Checkwin
         win = checkwin(arr, grid_size);
     }
     // Print Winner
-    printf("WINNER %c\n", get_player_char(win));
+    printf("\n\n\n\t\tWINNER %c\n\n\n", get_player_char(win));
 }
 
 int main(void)
